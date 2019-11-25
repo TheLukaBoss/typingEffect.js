f014f4388456f4f97923dd1df95d5bb727b9fdd8
Luka S <58115698+TheLukaBoss@users.noreply.github.com>
2019-11-25 17:48:54 +0000

    Made Syntax Better

--- unMinified.js
+++ unMinified.js
@@ -16,24 +16,24 @@ function typeWrite() {
 	
     getAllElementsWithAttribute("addTypingEffect").forEach(function(x) {
         window.animateData.push([x, x.innerHTML, (parseFloat(x.getAttribute("typingSpeed")) * 1000), getIndicesOf("<br>", x.innerHTML, !1), x.getAttribute("typingLoop"), JSON.parse(x.getAttribute("typingStopAllow").toLowerCase()),x.getAttribute("typingBackspace")]);
-        if (animateData[animateData.length - 1][4] == "false") {
-            animateData[animateData.length - 1][4] = !1
+        if (window.animateData[window.animateData.length - 1][4] == "false") {
+            window.animateData[window.animateData.length - 1][4] = !1;
         } else {
-            animateData[animateData.length - 1][4] = parseFloat(animateData[animateData.length - 1][4]) * 1000
+           window.animateData[window.animateData.length - 1][4] = parseFloat(window.animateData[window.animateData.length - 1][4]) * 1000;
         }
-		if (animateData[animateData.length - 1][6] == "false") {
-            animateData[animateData.length - 1][6] = !1
+		if (window.animateData[window.animateData.length - 1][6] == "false") {
+            window.animateData[window.animateData.length - 1][6] = !1;
         } else {
-            animateData[animateData.length - 1][6] = parseFloat(animateData[animateData.length - 1][6]) * 1000
+            window.animateData[window.animateData.length - 1][6] = parseFloat(window.animateData[window.animateData.length - 1][6]) * 1000;
         }
 		
         if (JSON.parse(x.getAttribute("typingKeepHeight").toLowerCase())) {
-            x.style.height = parseFloat(getComputedStyle(x, null).height.replace("px", ""))+"px"
+            x.style.height = parseFloat(getComputedStyle(x, null).height.replace("px", ""))+"px";
         }
     });
-    animateData.forEach(function(x, index) {
-		document.getElementById(x[0]["id"]).setAttribute("typingAnimating","true");
-        if (x[4] == !1) {
+    window.animateData.forEach(function(x, index) {
+		document.getElementById(x[0].id).setAttribute("typingAnimating","true");
+        if (x[4] == false) {
             x[0].innerHTML = "";
             window.i[index] = 0;
             window.clock[index] = setInterval(function() {
@@ -41,65 +41,65 @@ function typeWrite() {
                     clearInterval(window.clock[index]);
                     setTimeout(function() {
                         if (typeof typingEffectStopped === "function") {
-							document.getElementById(x[0]["id"]).setAttribute("typingAnimating","false");
-                            typingEffectStopped(x[0])
+							document.getElementById(x[0].id).setAttribute("typingAnimating","false");
+                            typingEffectStopped(x[0]);
                         } else {
-                            console.warn("Create a function called 'typingEffectStopped(element)' to execute code once the effect has stopped.")
+                            console.warn("Create a function called 'typingEffectStopped(element)' to execute code once the effect has stopped.");
                         }
-                    }, 10)
+                    }, 10);
                 } else if (x[5] == false && document.getElementById(x[0].id).getAttribute("addTypingEffect") == null) {
-                    console.warn("This element was told to stop, however the typingStopAllow attribute was set to 'false'. This writing effect has not stopped.")
+                    console.warn("This element was told to stop, however the typingStopAllow attribute was set to 'false'. This writing effect has not stopped.");
                 }
-                if (i[index] < x[1].length) {
-                    if (x[3].includes(i[index])) {
+                if (window.i[index] < x[1].length) {
+                    if (x[3].includes(window.i[index])) {
                         x[0].innerHTML += "<br>";
-                        i[index] += 4
+                        window.i[index] += 4;
                     } else {
-                        x[0].innerHTML += x[1].charAt(i[index]);
-                        i[index]++
+                        x[0].innerHTML += x[1].charAt(window.i[index]);
+                        window.i[index]++;
                     }
                 } else {
-                    clearInterval(clock)
+                    clearInterval(window.clock);
                 }
-            }, x[2])
+            }, x[2]);
         } else {
             loopPrintText(x, index);
             window.outerClock[index] = setInterval(function() {
                 loopPrintText(x, index);
-            }, x[4] + (x[2] * (x[1].length)) + (x[6]==false?0:(x[2] * (x[1].length))+x[6]))
+            }, x[4] + (x[2] * (x[1].length)) + (x[6]==false?0:(x[2] * (x[1].length))+x[6]));
         }
     });
 }
 
 function backspaceAll(x,index){
-	//console.log("hey");
 	var y = x[1].length;
+	var str;
 	setTimeout(function(){
 		window.backspaceClock[index] = setInterval(function(){
 			if (x[5] && document.getElementById(x[0].id).getAttribute("addTypingEffect") == null){
 				clearInterval(window.backspaceClock);
 				setTimeout(function(){
 					if (typeof typingEffectStopped === "function"){
-						document.getElementById(x[0]["id"]).setAttribute("typingAnimating","false");
-						typingEffectStopped(x[0])
+						document.getElementById(x[0].id).setAttribute("typingAnimating","false");
+						typingEffectStopped(x[0]);
 					} else {
-						console.warn("Create a function called 'typingEffectStopped(element)' to execute code once the effect has stopped.")
+						console.warn("Create a function called 'typingEffectStopped(element)' to execute code once the effect has stopped.");
 					}
-				}, 10)
+				}, 10);
 			} else if (x[5] == false && document.getElementById(x[0].id).getAttribute("addTypingEffect") == null) {
-				console.warn("This element was told to stop, however the typingStopAllow attribute was set to 'false'. This writing effect has not stopped.")
+				console.warn("This element was told to stop, however the typingStopAllow attribute was set to 'false'. This writing effect has not stopped.");
 			}
 			str = document.getElementById(x[0].id).innerHTML;
 			str = str.substring(0, str.length - 1);
-			document.getElementById(x[0]["id"]).innerHTML=str;
+			document.getElementById(x[0].id).innerHTML=str;
 			if(y == 0){
 				clearInterval(window.backspaceClock);
 			}
 			else{
 				y--;
 			}
-		},x[2])
-	},x[6])
+		},x[2]);
+	},x[6]);
 }
 
 function stopAnimation(elementId){
@@ -109,7 +109,7 @@ function stopAnimation(elementId){
 	}
 	else
 	{
-		console.log("Element is not being currently animated.")
+		console.log("Element is not being currently animated.");
 	}
 }
 
@@ -122,30 +122,30 @@ function loopPrintText(x, index) {
             clearInterval(window.outerClock[index]);
             setTimeout(function() {
                 if (typeof typingEffectStopped === "function") {
-					document.getElementById(x[0]["id"]).setAttribute("typingAnimating","false");
-                    typingEffectStopped(x[0])
+					document.getElementById(x[0].id).setAttribute("typingAnimating","false");
+                    typingEffectStopped(x[0]);
                 } else {
-                    console.warn("Create a function called 'typingEffectStopped(element)' to execute code once the effect has stopped.")
+                    console.warn("Create a function called 'typingEffectStopped(element)' to execute code once the effect has stopped.");
                 }
-            }, 10)
+            }, 10);
         } else if (x[5] == false && document.getElementById(x[0].id).getAttribute("addTypingEffect") == null) {
-            console.warn("This element was told to stop, however the typingStopAllow attribute was set to 'false'. This writing effect has not stopped.")
+            console.warn("This element was told to stop, however the typingStopAllow attribute was set to 'false'. This writing effect has not stopped.");
         }
-        if (i[index] < x[1].length) {
-            if (x[3].includes(i[index])) {
+        if (window.i[index] < x[1].length) {
+            if (x[3].includes(window.i[index])) {
                 x[0].innerHTML += "<br>";
-                i[index] += 4
+                window.i[index] += 4;
             } else {
-                x[0].innerHTML += x[1].charAt(i[index]);
-                i[index]++
+                x[0].innerHTML += x[1].charAt(window.i[index]);
+                window.i[index]++;
             }
         } else {
-            clearInterval(clock);
+            window.clearInterval(window.clock);
 			if (x[6]!=false){
 				backspaceAll(x,index);
 			}
         }
-    }, x[2])
+    }, x[2]);
 }
 
 function getAllElementsWithAttribute(attribute) {
@@ -153,26 +153,26 @@ function getAllElementsWithAttribute(attribute) {
     var allElements = document.getElementsByTagName('*');
     for (var i = 0, n = allElements.length; i < n; i++) {
         if (allElements[i].getAttribute(attribute) !== null) {
-            matchingElements.push(allElements[i])
+            matchingElements.push(allElements[i]);
         }
     }
-    return matchingElements
+    return matchingElements;
 }
 
 function getIndicesOf(searchStr, str, caseSensitive) {
     var searchStrLen = searchStr.length;
     if (searchStrLen == 0) {
-        return []
+        return [];
     }
     var startIndex = 0,
         index, indices = [];
     if (!caseSensitive) {
         str = str.toLowerCase();
-        searchStr = searchStr.toLowerCase()
+        searchStr = searchStr.toLowerCase();
     }
     while ((index = str.indexOf(searchStr, startIndex)) > -1) {
         indices.push(index);
-        startIndex = index + searchStrLen
+        startIndex = index + searchStrLen;
     }
-    return indices
+    return indices;
 }
